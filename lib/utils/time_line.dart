10389944300840ea19e2be1511a8204119a7f2d8
simonkimi <github@z31.mozmail.com>
2021-12-24 01:36:09 +0800

    新增时间轴计算函数时间

---
+++ lib/utils/time_line.dart
@@ -0,0 +1,12 @@
+class TimeLine {
+  final _timerMap = <String, int>{'_': DateTime.now().millisecondsSinceEpoch};
+
+  void startTimer(String? tag) =>
+      _timerMap[tag ?? '_'] = DateTime.now().millisecondsSinceEpoch;
+
+  void getRunTime({String tag = '_', String msg = '计时'}) {
+    final nowTime = DateTime.now().millisecondsSinceEpoch;
+    print('$msg 耗时: ${nowTime - (_timerMap[tag] ?? nowTime)} ms');
+    _timerMap[tag] = nowTime;
+  }
+}
