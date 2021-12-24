4f9ed7911895287a3f01edcdccda742b810ce15d
simonkimi <github@z31.mozmail.com>
2021-12-24 21:23:58 +0800

    修复加载信息藏在tabbar下面

--- lib/data/constant.dart
+++ lib/data/constant.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kLightGrey = Color(0xfff2f3f5);
 const kNavigatorBarHeight = 40.0;
 const kTabBarHeight = 30.0;
+const kBottomBarHeight = 50.0;
--- lib/ui/fragments/viewer_subpage/list/subpage_list.dart
+++ lib/ui/fragments/viewer_subpage/list/subpage_list.dart
@@ -78,61 +78,59 @@ class _SubPageListFragmentState extends State<SubPageListFragment> {
   }
 
   Widget _buildIndicator(BuildContext context) {
-    return CustomFooter(builder: (context, state) {
-      return Padding(
-        padding: widget.hasTabBar
-            ? const EdgeInsets.only(bottom: 50)
-            : EdgeInsets.zero,
-        child: () {
-          switch (state) {
-            case null:
-            case LoadStatus.canLoading:
-            case LoadStatus.idle:
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    CupertinoIcons.arrow_down,
-                    size: 18,
-                    color: CupertinoColors.label.resolveFrom(context),
-                  ),
-                  const SizedBox(width: 10),
-                  const Text('不要停下来啊!', style: TextStyle(fontSize: 15)),
-                ],
-              );
-            case LoadStatus.loading:
-              return SizedBox(
-                height: kNavigatorBarHeight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    CupertinoActivityIndicator(),
-                    SizedBox(width: 10),
-                    Text('努力加载中...', style: TextStyle(fontSize: 15)),
-                  ],
-                ),
-              );
-            case LoadStatus.noMore:
-              return const SizedBox(
-                height: kNavigatorBarHeight,
-                child: Center(
-                  child: Text('真的一点也没有了...', style: TextStyle(fontSize: 15)),
-                ),
-              );
-            case LoadStatus.failed:
-              return SizedBox(
-                height: kNavigatorBarHeight,
-                child: Center(
-                  child: Text(controller.errorMessage ?? '',
-                      style: const TextStyle(fontSize: 15)),
-                ),
-              );
-          }
-        }(),
-      );
-    });
+    return CustomFooter(
+      height: 50 + (widget.hasTabBar ? kBottomBarHeight : 0),
+      builder: _buildFootState,
+    );
+  }
+
+  Widget _buildFootState(BuildContext context, LoadStatus? state) {
+    switch (state) {
+      case null:
+      case LoadStatus.canLoading:
+      case LoadStatus.idle:
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              CupertinoIcons.arrow_down,
+              size: 18,
+              color: CupertinoColors.label.resolveFrom(context),
+            ),
+            const SizedBox(width: 10),
+            const Text('不要停下来啊!', style: TextStyle(fontSize: 15)),
+          ],
+        );
+      case LoadStatus.loading:
+        return SizedBox(
+          height: kNavigatorBarHeight,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              CupertinoActivityIndicator(),
+              SizedBox(width: 10),
+              Text('努力加载中...', style: TextStyle(fontSize: 15)),
+            ],
+          ),
+        );
+      case LoadStatus.noMore:
+        return const SizedBox(
+          height: kNavigatorBarHeight,
+          child: Center(
+            child: Text('真的一点也没有了...', style: TextStyle(fontSize: 15)),
+          ),
+        );
+      case LoadStatus.failed:
+        return SizedBox(
+          height: kNavigatorBarHeight,
+          child: Center(
+            child: Text(controller.errorMessage ?? '',
+                style: const TextStyle(fontSize: 15)),
+          ),
+        );
+    }
   }
 
   @override
