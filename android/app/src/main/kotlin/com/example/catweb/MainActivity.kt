15c9ca15ab3fbb44e95b1d4591c74e749f496002
simonkimi <git@z31.mozmail.com>
2023-06-19 18:23:54 +0800

    修复ios目录问题

--- android/app/src/main/kotlin/com/example/catweb/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/catweb/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ios
+package com.example.catweb
 
 import io.flutter.embedding.android.FlutterActivity
 
--- android/app/src/main/kotlin/ink/z31/catweb/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ink.z31.catweb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
