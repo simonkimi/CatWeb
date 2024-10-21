acaf9714b5afb89ffebf0e6aa52a17f0824ece4a
simonkimi <git@z31.mozmail.com>
2024-10-21 17:52:49 +0800

    移除parser

--- parser/example/android/app/src/main/kotlin/com/example/parser_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.parser_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- parser/ios/Classes/parser.c
+++
@@ -1,3 +0,0 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../parser.podspec for more information.
-#include "../../src/parser.c"
